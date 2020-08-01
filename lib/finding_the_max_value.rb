a701f4c18cec31b75a438977a46f8dcc00356a1f
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-01 03:49:17 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -6,11 +6,9 @@ def find_max_value(array)
   max = array[0]
   for i in 0..(array.length)
     puts max
-    if array[i] > 1
+    if array[i] > max
       max = array[i]
     end
   end
   max
 end
-
-find_max_value([1, 2, 3, 4])
