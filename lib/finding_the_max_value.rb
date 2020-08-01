4766a412c634f2381c35279720cbbc176baad3f1
Jessica Salbert <jessicarsalbert@gmail.com>
2020-08-01 03:50:56 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,7 @@
 def find_max_value(array)
-  if array.length == nil
-    return nil
-  end
-  
   max = array[0]
-  for i in 0..(array.length)
-    puts max
-    if array[i] > 1
+  for i in 0...(array.length)
+    if array[i] > max
       max = array[i]
     end
   end
